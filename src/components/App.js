984c615972b7cbd6b020bbd16fce91510374c55a
JIMMY JACOB <68079806+jimjacob29@users.noreply.github.com>
2020-10-21 15:01:39 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div>project-name</div>
+        <div>project-description</div>
+      </>
+    );
+  }
 }
-
 
 export default App;
